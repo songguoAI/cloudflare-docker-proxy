9a4cd492fe824bac3373bd526903a4108d9f7f3b
songguoAI <403551663@qq.com>
2024-08-19 04:37:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.docker-proxy.songguobingzi.cn": dockerHub,
+  "quay.docker-proxy.songguobingzi.cn": "https://quay.io",
+  "gcr.docker-proxy.songguobingzi.cn": "https://gcr.io",
+  "k8s-gcr.docker-proxy.songguobingzi.cn": "https://k8s.gcr.io",
+  "k8s.docker-proxy.songguobingzi.cn": "https://registry.k8s.io",
+  "ghcr.docker-proxy.songguobingzi.cn": "https://ghcr.io",
+  "cloudsmith.docker-proxy.songguobingzi.cn": "https://docker.cloudsmith.io",
+  "ecr.docker-proxy.songguobingzi.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.docker-proxy.songguobingzi.cn": dockerHub,
 };
 
 function routeByHosts(host) {
